2677adccfde8c4958894016f8ef5c5df829afcda
emmallado <60939882+emmallado@users.noreply.github.com>
2020-04-25 11:52:17 +0200

    menu

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,19 +1,9 @@
-AOS.init();
-
-$('.js-menu').on('click', function() {
-    $('html').toggleClass('menu-open');
-});
-
-$('.js-closeMenu').on('click', function() {
-    $('html').removeClass('menu-open');
-});
-
-var swiper = new Swiper('.swiper-container', {
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
-    pagination: {
-        el: '.swiper-pagination',
-    },
+$(document).ready(function () {
+    $('.js-open').on('click', function () {
+      $('html').addClass('menu-open');
+    });
+  
+    $('.js-close').on('click', function () {
+      $('html').removeClass('menu-open');
+    });
 });
